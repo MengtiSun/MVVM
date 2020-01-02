c6ac6cd239c3af2a53552af367905f7b29d40e06
Mengti Sun <mengtisun@gmail.com>
2020-01-03 10:52:04 +1100

    proxy data

--- js/MVVM.js
+++ js/MVVM.js
@@ -7,8 +7,21 @@ class MVVM {
     // if the template exits, I want to do something fancy
     if (this.$el) {
       new Observer(this.$data);
+      this.proxyData(this.$data);
       // This is a template engine
       new Compile(this.$el, this);
     }
+  }
+  proxyData(data) {
+    Object.keys(data).forEach((key)=> {
+      Object.defineProperty(this, key, {
+        get() {
+          return data[key];
+        },
+        set(newValue) {
+          data[key] = newValue;
+        }
+      })
+    })
   }
 }
